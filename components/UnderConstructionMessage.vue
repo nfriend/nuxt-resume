1b809ffa9dfbb208b668bfff07391d275f1946bb
Nathan Friend <nathan@gitlab.com>
2020-02-19 09:39:57 -0500

    Add under construction message

---
+++ components/UnderConstructionMessage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="text-gray-700">
+    <p>
+      <strong>This site is under construction! 🛠️</strong>
+    </p>
+    <p>
+      In the meantime, check out the source at
+      <a
+        class="text-blue-500 hover:text-blue-600 hover:underline"
+        href="https://gitlab.com/nfriend/nuxt-resume"
+        >GitLab.</a
+      >
+    </p>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="container min-h-screen flex flex-col mx-auto">
+  <div
+    class="container min-h-screen flex flex-col mx-auto shadow-2xl p-20 m-20 bg-white"
+  >
     <Header />
+    <UnderConstructionMessage class="pb-4" />
     <DeploymentInfo />
   </div>
 </template>
@@ -8,17 +11,21 @@
 <script>
 import Header from '~/components/Header.vue';
 import DeploymentInfo from '~/components/DeploymentInfo.vue';
+import UnderConstructionMessage from '~/components/UnderConstructionMessage';
 
 export default {
   components: {
     Header,
     DeploymentInfo,
+    UnderConstructionMessage,
   },
 };
 </script>
 
-<style>
-body {
+<style lang="postcss">
+body,
+html {
   font-family: 'Bitter', serif;
+  @apply bg-gray-200;
 }
 </style>
