4cb1955861745bbd2ca933c364d15251f6fcbc91
Nathan Friend <nathan@gitlab.com>
2020-02-19 13:05:37 -0500

    Move main layout into own component

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,12 @@
 <template>
-  <div
-    class="container font-serif min-h-screen flex flex-col mx-auto shadow-2xl p-20 m-20 bg-ivory"
-  >
-    <Header />
-    <UnderConstructionMessage class="pb-4" />
-    <DeploymentInfo />
-  </div>
+  <Resume />
 </template>
 
 <script>
-import Header from '~/components/Header.vue';
-import DeploymentInfo from '~/components/DeploymentInfo.vue';
-import UnderConstructionMessage from '~/components/UnderConstructionMessage';
+import Resume from '~/components/Resume.vue';
 
 export default {
-  components: {
-    Header,
-    DeploymentInfo,
-    UnderConstructionMessage,
-  },
+  components: { Resume },
 };
 </script>
 
