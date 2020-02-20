363836fc98f74312f7e8602b842ad8d718def233
Nathan Friend <nathan@gitlab.com>
2020-02-20 10:40:57 -0500

    Update page title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Nathan Friend: Résumé',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
