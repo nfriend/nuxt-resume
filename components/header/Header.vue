160d213c9391c433716e1bcb96e1a2fd5bd1eed5
Nathan Friend <nathan@gitlab.com>
2020-02-21 21:56:24 -0500

    Add "View as PDF" button

--- components/header/Header.vue
+++ components/header/Header.vue
@@ -1,9 +1,12 @@
 <template>
   <header>
-    <h1 class="flex-grow flex-shrink-0">
-      <Kerned :letters="title" />
-    </h1>
-    <section class="flex-shink-0">
+    <div class="flex items-start">
+      <h1 class="flex-grow flex-shrink-0">
+        <Kerned :letters="title" />
+      </h1>
+      <PdfLink />
+    </div>
+    <section>
       <ContactInfo />
     </section>
   </header>
@@ -12,10 +15,11 @@
 <script>
 import Kerned from '../utility/Kerned';
 import ContactInfo from './ContactInfo';
+import PdfLink from './PdfLink';
 import { resumeData } from '~/resume-data';
 
 export default {
-  components: { Kerned, ContactInfo },
+  components: { Kerned, ContactInfo, PdfLink },
   computed: {
     title() {
       return resumeData.title;
---
+++ components/header/PdfLink.vue
@@ -0,0 +1,17 @@
+<template>
+  <a
+    class="btn flex items-center bg-white hover:bg-gray-100 active:bg-gray-200 text-gray-600 hover:text-gray-700 active:text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+    href="/Nathan Friend - Résumé.pdf"
+  >
+    <Icon type="pdf" class="mr-1" />
+    View as PDF
+  </a>
+</template>
+
+<script>
+import Icon from '~/components/utility/Icon';
+
+export default {
+  components: { Icon },
+};
+</script>
