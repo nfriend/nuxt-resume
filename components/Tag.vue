8ad897343d8330ee2f01b1d790346eb4f3fe3861
Nathan Friend <nathan@gitlab.com>
2020-02-23 13:57:10 -0500

    Add Tag component

---
+++ components/Tag.vue
@@ -0,0 +1,31 @@
+<template>
+  <a
+    v-if="link"
+    :href="link"
+    class="tag hover:bg-gray-400 hover:text-gray-800 active:bg-gray-500 active:text-gray-900"
+  >
+    <slot></slot>
+  </a>
+  <span v-else class="tag">
+    <slot></slot>
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    link: {
+      type: String,
+      required: false,
+      default: null,
+    },
+  },
+};
+</script>
+
+<style lang="postcss" scoped>
+a,
+span {
+  @apply text-gray-700 bg-gray-300 text-sm px-2 py-1 mr-1;
+}
+</style>
