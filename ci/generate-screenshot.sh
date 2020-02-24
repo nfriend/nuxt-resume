9a1d23c21a27d88fa0684e902eb15d43ef04b65d
Nathan Friend <nathan@gitlab.com>
2020-02-24 10:26:38 -0500

    Testing with new container

--- ci/generate-screenshot.sh
+++ ci/generate-screenshot.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-chromium-browser --headless \
-                 --disable-gpu \
-                 --disable-software-rasterizer \
-                 --disable-dev-shm-usage \
-                 --no-sandbox \
-                 --screeenshot="screenshot.png" \
-                 --hide-scrollbars \
-                 --window-size=1200,630 \
-                 --enable-logging=stderr \
-                 --v=1 \
-                 file://$CI_PROJECT_DIR/dist/index.html
+# chromium-browser --headless \
+#                  --disable-gpu \
+#                  --disable-software-rasterizer \
+#                  --disable-dev-shm-usage \
+#                  --no-sandbox \
+#                  --screeenshot="screenshot.png" \
+#                  --hide-scrollbars \
+#                  --window-size=1200,630 \
+#                  file://$CI_PROJECT_DIR/dist/index.html
+
+echo "Do I need this?"
