3402672e586a3b1e912f53db1fcc9b2a4dbf361b
Nathan Friend <nathan@gitlab.com>
2020-02-24 09:31:17 -0500

    PNG, not PDF

--- ci/generate-screenshot.sh
+++ ci/generate-screenshot.sh
@@ -3,7 +3,7 @@
 chromium-browser --headless \
                  --disable-gpu \
                  --no-sandbox \
-                 --screeenshot="screenshot.pdf" \
+                 --screeenshot="screenshot.png" \
                  --window-size=1200,630 \
                  --hide-scrollbars \
                  file://$CI_PROJECT_DIR/dist/index.html
