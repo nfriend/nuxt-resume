499c6b65e4786d3d7f4fdae279a6be14b5fc0e23
Nathan Friend <nathan@gitlab.com>
2020-02-24 09:15:20 -0500

    Add trailing slash

--- ci/generate-screenshot.sh
+++ ci/generate-screenshot.sh
@@ -6,6 +6,6 @@ chromium-browser --headless \
                  --disable-dev-shm-usage \
                  --no-sandbox \
                  --screeenshot="screenshot.pdf" \
-                 --window-size=1200,630
+                 --window-size=1200,630 \
                  --hide-scrollbars \
                  file://$CI_PROJECT_DIR/dist/index.html
