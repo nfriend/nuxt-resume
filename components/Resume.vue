67ee43d82d5a387bdc230b304d6a1fd861f5b1bf
Nathan Friend <nathan@gitlab.com>
2020-02-26 20:27:40 -0500

    Move deployment info below paper

--- components/Resume.vue
+++ components/Resume.vue
@@ -1,20 +1,25 @@
 <template>
   <div
-    class="paper-page
-           flex flex-col
+    class="flex flex-col items-stretch
+           mx-auto
            w-full paper:w-paper print:w-print
-           h-auto paper:h-paper print:h-paper
-           mx-auto my-0 paper:my-20 print:my-0
-           p-4 sm:p-10 paper:p-20 print:p-20
-           text-xl md:text-base
-           shadow-none paper:shadow-2xl print: shadow-none
-           bg-ivory print:bg-transparent
            transform xl:scale-120 2xl:scale-10 3xl:scale-160 origin-top"
   >
-    <Header />
-    <!-- <UnderConstructionMessage v-if="isProduction" /> -->
-    <PlaceholderContent class="flex-grow" />
-    <DeploymentInfo />
+    <div
+      class="paper-page
+           flex flex-col
+           h-auto paper:h-paper print:h-paper
+           my-0 paper:my-20 print:my-0
+           p-4 sm:p-10 paper:p-20 print:p-20
+           text-xl md:text-base
+           shadow-none paper:shadow-2xl print:shadow-none
+           bg-ivory print:bg-transparent"
+    >
+      <Header />
+      <!-- <UnderConstructionMessage v-if="isProduction" /> -->
+      <PlaceholderContent class="flex-grow" />
+    </div>
+    <DeploymentInfo class="self-center mb-10" />
   </div>
 </template>
 
