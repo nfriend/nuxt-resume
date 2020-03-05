b542e91808afa01ffef6ee8b8831bc73b70fed29
Nathan Friend <nathan@gitlab.com>
2020-03-05 11:32:36 -0500

    Attempting to fix artifact paths in CI

--- ci/generate-pdf.sh
+++ ci/generate-pdf.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-cd $CI_PROJECT_DIR/dist
-
 chromium-browser --headless \
                  --disable-gpu \
                  --disable-software-rasterizer \
                  --disable-dev-shm-usage \
                  --no-sandbox \
-                 --print-to-pdf="'Nathan Friend - Résumé.pdf'" \
+                 --print-to-pdf="$CI_PROJECT_DIR/dist/Nathan Friend - Résumé.pdf" \
                  --hide-scrollbars \
                  file://$CI_PROJECT_DIR/dist/index.html
--- ci/run-html-proofer.rb
+++ ci/run-html-proofer.rb
@@ -7,6 +7,7 @@ require 'html-proofer'
 
 dist_dir = File.expand_path(File.join(__dir__, '../dist'))
 HTMLProofer.check_directory(dist_dir, {
+  :url_ignore => ['https://www.linkedin.com/in/nfriend/'],
   :check_html => true,
   :check_opengraph => true,
   :check_favicon => true,
