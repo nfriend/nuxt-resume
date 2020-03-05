6c1db64240259e744486e56229d59b7709a4ed07
Nathan Friend <nathan@gitlab.com>
2020-03-05 09:48:35 -0500

    Move html linting call into .gitlab-ci.yml

--- ci/run-html-proofer.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-# frozen_string_literal: true
-# encoding: utf-8
-
-require 'html-proofer'
-
-dist_dir = File.expand_path(File.join(__dir__, '../dist'))
-HTMLProofer.check_directory(dist_dir).run
