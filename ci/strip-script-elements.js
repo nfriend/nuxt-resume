384fd65f524cf0c785fc47c29572ff8824680b55
Nathan Friend <nathan@gitlab.com>
2020-03-10 11:13:43 -0400

    Strip all <script> elements from output HTML

---
+++ ci/strip-script-elements.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+/* eslint-disable no-console */
+
+const fs = require('fs');
+const path = require('path');
+const cheerio = require('cheerio');
+
+const indexFilePath = path.join(__dirname, '../dist/index.html');
+console.log(`Loading HTML from ${indexFilePath}...`);
+const indexHtml = fs.readFileSync(indexFilePath, 'utf8');
+
+console.log('Stripping all <script> elements...');
+const $ = cheerio.load(indexHtml);
+$('script').remove();
+
+console.log(`Overwriting ${indexFilePath}...`);
+fs.writeFileSync(indexFilePath, $.html());
